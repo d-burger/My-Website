89e788316388aed99dfb0d920935e18e557b3e51
d-burger <dominique.burger.de@gmai.com>
2022-02-03 18:18:47 +0100

    Beta Version 1.0

--- app.js
+++ app.js
@@ -1,20 +1,44 @@
 // -------------- SELECTORS AND VARIABLES --------------
+const html = document.querySelector("html");
 const burger = document.querySelector(".burger");
 const overlay = document.querySelector(".overlay-nav");
 const closeBtn = document.querySelector(".close");
 const navLinksMobile = document.querySelectorAll(".list-item-m a");
+const contactFormMobile = document.querySelector(".form-mbl-container");
+const contactMeBtnMobile = document.querySelector(".envelope");
+const closeFormBtn = document.querySelector(".close-form");
+const submitFormBtn = document.querySelector(".mobile-form-submit-button");
 
 // -------------- EVENT LISTENERS ----------------------
+// BURGER
 burger.addEventListener("click", () => {
-  overlay.classList.add("visible");
+  overlay.classList.toggle("visible");
+  html.classList.toggle("no-scroll");
 });
 
 closeBtn.addEventListener("click", () => {
-  overlay.classList.remove("visible");
+  overlay.classList.toggle("visible");
+  html.classList.toggle("no-scroll");
 });
 
 navLinksMobile.forEach((link) => {
   link.addEventListener("click", () => {
-    overlay.classList.remove("visible");
+    overlay.classList.toggle("visible");
+    html.classList.toggle("no-scroll");
   });
+});
+
+// MOBILE CONTACT FORM
+contactMeBtnMobile.addEventListener("click", () => {
+  contactFormMobile.classList.toggle("visible-form");
+  html.classList.toggle("no-scroll");
+});
+
+closeFormBtn.addEventListener("click", () => {
+  contactFormMobile.classList.toggle("visible-form");
+  html.classList.toggle("no-scroll");
+});
+submitFormBtn.addEventListener("click", () => {
+  contactFormMobile.classList.toggle("visible-form");
+  html.classList.toggle("no-scroll");
 });
